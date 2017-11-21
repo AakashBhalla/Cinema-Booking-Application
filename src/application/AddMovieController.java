586ebbb331a44ee7976d229522bc6b3d1c2c38f7
AakashBhalla <aakash.bhalla.13@ucl.ac.uk>
2017-11-21 02:14:25 +0000

    removed import not needed

--- src/application/AddMovieController.java
+++ src/application/AddMovieController.java
@@ -22,7 +22,6 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
